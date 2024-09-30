1a730d8d693d3f37ba567b45e5402c4beda16256
juandanielchoque <juan.choque.m@tecsup.edu.pe>
2024-09-30 18:02:12 -0500

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by juan choque on 30/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
